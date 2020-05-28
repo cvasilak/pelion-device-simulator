40391dfdda8cab2ca22ae093b4b3efc538799b43
Christos Vasilakis <christos.vasilakis@arm.com>
2020-05-28 14:46:42 +0100

    check if previous compiled and skip the step.

--- peliondm/build-pelion-and-launch.js
+++ peliondm/build-pelion-and-launch.js
@@ -1,32 +1,48 @@
 const launchServer = require('./server/launch-server');
 const Path = require('path');
+const fs = require('fs');
 
 const child_process = require('child_process');
 const program = require('commander');
 
 const demo_output_directory = 'out';
 
-console.log(`Building peliondm-example ...`);
-
 program.parse(process.argv);
 
-const buildResult = child_process.spawnSync(
-    'node',
-    [
-        'cli.js',
-        '-i',
-        'demos/peliondm',
-        '-o',
-        `${demo_output_directory}`,
-        '--compiler-opts',
-        '-Os'
-    ]
-);
+// if not already compiled, kickstart certificate generation and compilation
+if (!fs.existsSync(`${demo_output_directory}/peliondm.wasm`)) {
+    console.log(`NOTE: first-run detected, need to generate dev certificate and build pelion-demo (subsequent invocations won't ne ed it) ...\n`);
+    
+    console.log(`Generating credentials file ...`);
+    const genCertResult = child_process.spawnSync(
+        'python', ['generate-certificate.py'], { cwd : 'demos/peliondm' }
+    );
 
-if (buildResult.status === 0) {
-    console.log(buildResult.stdout.toString());
-} else {
-    console.log(buildResult.stderr.toString());
+    if (genCertResult.status === 0) {
+        console.log(genCertResult.stdout.toString());
+    } else {
+        console.log(genCertResult.stderr.toString());
+    }
+
+    console.log(`Building 'pelion-demo' ...`);
+    const buildResult = child_process.spawnSync(
+        'node',
+        [
+            'cli.js',
+            '-i',
+            'demos/peliondm',
+            '-o',
+            `${demo_output_directory}`,
+            '--compiler-opts',
+            '-Os'
+        ]
+    );
+
+    if (buildResult.status === 0) {
+        console.log(buildResult.stdout.toString());
+    } else {
+        console.log(buildResult.stderr.toString());
+    }
 }
 
 // launch Web Server
